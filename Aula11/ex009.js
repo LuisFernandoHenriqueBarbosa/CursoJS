0b3f419e9f803e6f5e9a4aa1a285ce88ebf9dcb6
Luis Fernando <luisfernandohenriquebarbosa18@hotmail.com>
2024-10-21 16:13:15 -0300

    pratica de condições

--- Aula11/ex009.js
+++ Aula11/ex009.js
@@ -1,8 +1,8 @@
-// var pais = 'Brasil'
-// console.log(`Vivendo em ${pais}`)
-// if (pais == 'Brasil'){
-//     console.log('Você é Brasileiro!')
-// }else{
-//     console.log('Você é Estrangeiro!')
-// }
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Você é Brasileiro!')
+}else{
+    console.log('Você é Estrangeiro!')
+}
 
