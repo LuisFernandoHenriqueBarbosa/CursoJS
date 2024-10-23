6e08967593c480933dbc2377bbcead421d661a1a
Luis Fernando <luisfernandohenriquebarbosa18@hotmail.com>
2024-10-22 22:06:59 -0300

    estrutura de repetição

---
+++ Aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1;
+do {
+  console.log(`Passo${c}`);
+  c++;
+} while (c <= 6);
+
+/*
+var c = 1
+while(c <= 6 ){
+    console.log(`Passo${c}`)
+    c++
+}
+*/
+
+/*
+console.log('Passo1')
+console.log('Passo2')
+console.log('Passo3')
+console.log('Passo4')
+console.log('Passo5')
+console.log('Passo6')
+*/
